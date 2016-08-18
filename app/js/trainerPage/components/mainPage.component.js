e8bae08257b62ae594752206b70ec452f1d69e41
halynakondratiuk <rosa.lovely23@gmail>
2016-08-18 18:58:36 +0300

    InitialCommit

---
+++ app/js/trainerPage/components/mainPage.component.js
@@ -0,0 +1,23 @@
+var angular = require('angular');
+
+module.exports = angular
+    .module('app.mainTrainerPage.module', [])
+    .component('mainPage',{
+        templateUrl:'/app/js/trainerPage/components/mainPage.template.html',
+        controller: MainPageController
+    })
+    .factory('MainPageFactory', ['$resource',
+        function($resource) {
+            return $resource('', {}, {
+                all: {
+                    method: "GET",
+                    url: ""
+                }
+            });
+        }
+    ]);
+
+function MainPageController ($scope, MainPageFactory) {
+    $scope.mainPage = MainPageFactory.all();
+};
+
---
+++ app/js/trainerPage/components/trainerPageDescription.component.js
@@ -0,0 +1,26 @@
+var angular = require('angular');
+
+module.exports = angular
+    .module('app.trainerDescriptionPage.module', [])
+    .component('trainerPage',{
+        templateUrl:'/app/js/trainerPage/components/trainerPageDescription.template.html',
+        controller: TrainingDescriptionController
+    })
+.factory('TrainerPageDescriptionFactory', ['$resource',
+    function($resource) {
+        return $resource('', {}, {
+            all: {
+                method: "GET",
+                url: ""
+            }
+        });
+    }
+]);
+
+function TrainingDescriptionController ($scope, TrainerPageDescriptionFactory) {
+    this.$routerOnActivate = function (next, previous) {
+        var id = next.params.id;
+        $scope.trainerPageDescription = TrainerPageDescriptionFactory.all({id: id});
+
+    }
+};
